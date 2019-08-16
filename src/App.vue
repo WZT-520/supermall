23980d52a421f378cf8c9425c5067a3692d1b466
wzt-520 <953912425@qq.com>
2019-08-16 12:20:13 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item path='/home' activeColor="deepPink">
+          <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+          <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg" alt="">
+          <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category' activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg" alt="">
+          <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/cart' activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
+         <img slot="item-icon-active" src="./assets/img/tabbar/shopcart_active.svg" alt="">
+          <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile' activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg" alt="">
+          <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+
+    <router-view/>
+  </div>
+</template>
+<script>
+ import TabBar from './components/tabbar/TabBar';
+ import TabBarItem from './components/tabbar/TabBaritem'
+export default {
+  data(){
+    return{}
+  },
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+<style lang="scss">
+ @import './assets/css/base.css';
+ 
+</style>
